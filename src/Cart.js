6b9bf758276235b85aa65277a292f33bc9a058f0
Harish Nishad <harish.nishad0775@gmail.com>
2021-09-01 23:10:56 +0530

    Handling increaseQuantity() in Cart || Part 2

--- src/Cart.js
+++ src/Cart.js
@@ -33,6 +33,12 @@ class Cart extends React.Component{
       }
       IncreaseQuantity = (product)=>{
         console.log('Hey Increase Quantity',product)
+        const{products} = this.state; // get all product of a State
+        const index = products.indexOf(product); // get index of the particular (Product) in the Products Array
+        products[index].qty += 1; // Inc the Qty of product of particular product(index)
+        this.setState({
+          products : products
+        })
       }
     render(){
         const{products}  = this.state;
