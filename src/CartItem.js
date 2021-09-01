fa5721f0f8c244ae3523e67eaa73e9aeadec0b29
Harish Nishad <harish.nishad0775@gmail.com>
2021-09-01 21:38:04 +0530

    Props

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,16 +1,7 @@
 import React from 'react';
 
 class CartItem extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      price : 999,
-      title : 'Telephone',
-      qty : 1,
-      img : '' 
-    }
-  
-  }
+
   increaseQuantity = () => {
      this.setState((prevState) => {
       return {
@@ -35,7 +26,8 @@ class CartItem extends React.Component {
 
   
   render () {
-  const { price,title,qty} = this.state;
+    console.log('this.props',this.props);
+     const { price,title,qty} = this.props.product;
     return (
       <div className="cart-item">
         <div className="left-block">
