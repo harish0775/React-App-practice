b70b043fdf47b7ddd5103364688f53db69871c91
Harish Nishad <harish.nishad0775@gmail.com>
2021-09-07 23:43:06 +0530

    add file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
-     
+ 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+git add .
+git commit -m"add file"
+git push
